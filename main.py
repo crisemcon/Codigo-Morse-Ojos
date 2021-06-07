e529b2fa92ea2900cadb373ee5bf0368c9318bbb
Cristian Contreras <crisemcon@gmail.com>
2021-06-07 18:24:17 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+import cv2
+import numpy as np
+import dlib
+
+cap = cv2.VideoCapture(0)
+
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+
+def midpoint(p1,p2):
+    return int((p1.x+p2.x)/2), int((p1.y+p2.y)/2)
+
+while True:
+    _, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = detector(gray)
+    for face in faces:
+        x1, y1 = face.left(), face.top()
+        x2, y2 = face.right(), face.bottom()
+        #cv2.rectangle(frame, (x1,y1), (x2,y2), (0,255,0), 2)
+
+        landmarks = predictor(gray, face)
+        left_point = (landmarks.part(36).x, landmarks.part(36).y)
+        right_point = (landmarks.part(39).x, landmarks.part(39).y)
+        top_point = midpoint(landmarks.part(37),landmarks.part(38))
+        bottom_point = midpoint(landmarks.part(41),landmarks.part(40))
+
+        hor_line = cv2.line(frame, left_point, right_point, (0,255,0), 1)
+        ver_line = cv2.line(frame, top_point, bottom_point, (0,255,0), 1)
+
+    cv2.imshow("Frame", frame)
+
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
